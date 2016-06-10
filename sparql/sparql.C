41ceb26df60e94727605ebe2a61d28ed214ecf05
Cyber MacGeddon <cybermaggedon@gmail.com>
2016-06-10 22:36:26 +0100

    SPARQL endpoint doesn't crash on query errors.  Cornish pasty recipe on d3sparql page.

--- sparql/sparql.C
+++ sparql/sparql.C
@@ -14,93 +14,113 @@
 #define STORE_NAME "http://localhost:8080/example-rest/v1"
 #endif
 
-#define PAGE "<html><head><title>libmicrohttpd demo</title>"	\
-    "</head><body>libmicrohttpd demo</body></html>"
+#ifndef BASE_URI
+#define BASE_URI "https://github.com/cybermaggedon/"
+#endif
 
 librdf_world* world;
 librdf_model* model;
+librdf_uri* base_uri;
 
+/* HTTP request handler. */
 static int request_handler(void* cls, struct MHD_Connection * connection,
 			   const char * url, const char * method,
 			   const char * version, const char * upload_data,
 			   size_t * upload_data_size, void ** ptr)
 {
     
-    const char * page = (char*) cls;
     struct MHD_Response * response;
     int ret;
 
+    /* This detects first call of the function when we have the HTTP header. 
+       We accept all headers. */
     if (*ptr) {
-	/* The first time only the headers are valid, don't respond. */
+	/* The first time we only have headers, don't respond. */
 	*ptr = (void*) 1;
 	return MHD_YES;
     }
 
-    const char* output = MHD_lookup_connection_value(connection,
-						     MHD_GET_ARGUMENT_KIND,
-						     "output");
-  
-    const char* query = MHD_lookup_connection_value(connection,
-						    MHD_GET_ARGUMENT_KIND,
-    						    "query");
-  
-    const char* cb = MHD_lookup_connection_value(connection,
-    						 MHD_GET_ARGUMENT_KIND,
-    						 "callback");
-  
-    if (0 != strcmp(method, "GET"))
-    	return MHD_NO; /* unexpected method */
-  
-    if (0 != *upload_data_size)
-    	return MHD_NO; /* upload data in a GET!? */
+    const char* cb;
+    const char* output;
+    const char* query;
     
-    *ptr = NULL; /* clear context pointer */
-  
-    librdf_uri* uri1 =
-    	librdf_new_uri(world, (const unsigned char*) "http://bunchy.org");
-
-    if (uri1 == 0)
-    	throw std::runtime_error("Couldn't parse URI");
-    
-    librdf_uri* uri2 =
-    	librdf_new_uri(world, (const unsigned char*) "http://bunchy.org");
-    if (uri2 == 0)
-    	throw std::runtime_error("Couldn't parse URI");
-    
-    librdf_query* qry =
-    	librdf_new_query(world, "sparql", uri1,
-    			 (const unsigned char*) query,
-    			 uri2);
-    
-    librdf_free_uri(uri1);
-    librdf_free_uri(uri2);
-    
-    if (qry == 0)
-    	throw std::runtime_error("Couldn't create query");
-
-    librdf_query_results* results = librdf_query_execute(qry, model);
-    if (results == 0)
-    	throw std::runtime_error("Couldn't execute query");
-    
-    librdf_free_query(qry);
-
-    size_t len = 0;
-    unsigned char* out;
+    librdf_query* qry = 0;
+    librdf_query_results* results = 0;
+    unsigned char* out = 0;
 
     std::string mime_type;
-    
-    if (output && strcmp(output, "json") == 0) {
-    	mime_type = "application/sparql-results+json";
-    	out = librdf_query_results_to_counted_string2(results, "json", 0, 0, 0,
-    						      &len);
-    } else {
-    	mime_type = "text/plain";  // FIXME
-    	out = librdf_query_results_to_counted_string2(results, 0, 0, 0, 0,
-						      &len);
-    }
 
-    if (out == 0)
-	throw std::runtime_error("Couldn't output query results as string");
+    size_t len = 0;
+
+    try {
+
+	/* Get arguments: output, query and callback. */
+	output = MHD_lookup_connection_value(connection,
+					     MHD_GET_ARGUMENT_KIND,
+					     "output");
+	query = MHD_lookup_connection_value(connection,
+					    MHD_GET_ARGUMENT_KIND,
+					    "query");
+	cb = MHD_lookup_connection_value(connection,
+					 MHD_GET_ARGUMENT_KIND,
+					 "callback");
+
+	/* Only respond to the GET method. */
+	if (0 != strcmp(method, "GET"))
+	    return MHD_NO; /* unexpected method */
+	
+	/* Flip out if GET has a payload. */
+	if (0 != *upload_data_size)
+	    return MHD_NO; /* upload data in a GET!? */
+	
+	*ptr = NULL; /* clear context pointer */
+	
+	qry = librdf_new_query(world, "sparql", 0,
+			       (const unsigned char*) query,
+			       base_uri);
+	if (qry == 0)
+	    throw std::runtime_error("Couldn't create query");
+	
+	results = librdf_query_execute(qry, model);
+	if (results == 0)
+	    throw std::runtime_error("Couldn't execute query");
+	
+	librdf_free_query(qry);
+	qry = 0;
+	
+	
+	if (output && strcmp(output, "json") == 0) {
+	    mime_type = "application/sparql-results+json";
+	    out = librdf_query_results_to_counted_string2(results, "json",
+							  0, 0, 0,
+							  &len);
+	} else {
+	    mime_type = "text/plain";  // FIXME
+	    out = librdf_query_results_to_counted_string2(results, 0, 0, 0, 0,
+							  &len);
+	}
+
+	if (out == 0)
+	    throw std::runtime_error("Couldn't output query results as string");
+	
+    } catch (std::exception& e) {
+
+	if (qry)
+	    librdf_free_query(qry);
+
+	if (results)
+	    librdf_free_query_results(results);
+
+	response = MHD_create_response_from_buffer(0, (void*) "",
+						   MHD_RESPMEM_MUST_COPY);
+
+	ret = MHD_queue_response(connection, MHD_HTTP_INTERNAL_SERVER_ERROR,
+				 response);
+	MHD_destroy_response(response);
+
+	return ret;
+
+    }
 
     if (cb != 0) {
 	int len2 = strlen(cb) + 2 + len;
@@ -116,7 +136,6 @@ static int request_handler(void* cls, struct MHD_Connection * connection,
 	response = MHD_create_response_from_buffer (len2, 
 						    (void*) out2,
 						    MHD_RESPMEM_MUST_COPY);
-
 	free(out2);
 	librdf_free_memory(out);
 	
@@ -125,7 +144,6 @@ static int request_handler(void* cls, struct MHD_Connection * connection,
 	response = MHD_create_response_from_buffer (len, 
 						    (void*) out,
 						    MHD_RESPMEM_MUST_COPY);
-
 	librdf_free_memory(out);
 
     }
@@ -160,7 +178,11 @@ int main(int argc, char ** argv)
     model = librdf_new_model(world, storage, 0);
     if (model == 0)
 	throw std::runtime_error("Couldn't construct model");
-
+    
+    base_uri =
+    	librdf_new_uri(world, (const unsigned char*) BASE_URI);
+    if (base_uri == 0)
+    	throw std::runtime_error("Couldn't parse URI.");
   
     struct MHD_Daemon * d;
 
